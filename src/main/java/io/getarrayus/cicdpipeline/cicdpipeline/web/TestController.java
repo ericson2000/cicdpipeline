31fc0e4f1b4b776bdb45308c665fce97f8cd0db2
ericwouwo <wouwoeric@gmail.com>
2023-11-06 17:08:10 +0100

    first commit

---
+++ src/main/java/io/getarrayus/cicdpipeline/cicdpipeline/web/TestController.java
@@ -0,0 +1,24 @@
+package io.getarrayus.cicdpipeline.cicdpipeline.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @version 1.0
+ * @Author Eric Wouwo Tionang
+ * @licence
+ */
+
+@RestController
+@RequestMapping(path = "/app")
+public class TestController {
+
+    @GetMapping(path = "/test")
+    public ResponseEntity<Map<String,String>> test(){
+        return ResponseEntity.ok().body(Map.of("Testing", "Up and running"));
+    }
+}
